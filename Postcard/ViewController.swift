9474185ed4c60b27ac5eaed90b5bdf37e0896d4c
Raihan Morshed <raihanmorshed@Raihans-MacBook-Pro.local>
2015-01-31 15:45:01 -0500

    first commit

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    
+    @IBOutlet weak var mailButton: UIButton!
+    //button as an outlet not an action
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +33,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: AnyObject) {
+    
+        
+        messageLabel.hidden = false
+        //unhide the label
+        messageLabel.text = enterMessageTextField.text
+        messageLabel.textColor = UIColor.redColor()
+        //changes the color of the label in code not in storyboard
+        
+        enterMessageTextField.text = " "
+        // puts back the placeholder text again into the text field
+        enterMessageTextField.resignFirstResponder()
+        //makes the keyboard go away with the re.. function
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        //button outlet. Control state allows developers to determine the current touch state of a Button.()arguments in them
+    
+    
+    
+    
+    }
+    
 
 }
-
